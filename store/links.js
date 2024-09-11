83e083cb68ead86a2af9222627d53668dba9588d
MeetStacee <meetstacee@gmail.com>
2024-09-10 21:06:41 -0500

    modified StaceeProjectCapstone

---
+++ store/links.js
@@ -0,0 +1,6 @@
+export default [
+    { url: "/home", text: "Home" },
+    { url: "/about-us", text: "About Us" },
+    { url: "/order", text: "Submit a Pizza Order" },
+    { url: "/blog", text: "Welcome To My Blog" }
+  ];
---
+++ views/index.js
@@ -0,0 +1,5 @@
+export { default as home } from "./home";
+export { default as aboutUs } from "./aboutUs";
+export { default as blog } from "./blog";
+export { default as order } from "./order";
+export { default as viewNotFound } from "./viewNotFound";
